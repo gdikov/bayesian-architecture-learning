c72e7826d98dabc8f700b65d61f27dfc166ba23b
Georgi Dikov <gvdikov@gmail.com>
2019-12-22 13:55:12 +0100

    refactor and fix many issues

--- bal/layers.py
+++ bal/layers.py
@@ -8,25 +8,43 @@ class AdaptiveSize(nn.Module):
     def __init__(self,
                  min_size,
                  max_size,
-                 prior_size_loc=1.0,
-                 prior_size_scale=1.0,
+                 prior_loc=1.0,
+                 prior_scale=1.0,
                  temperature=1.0):
         super(AdaptiveSize, self).__init__()
         self.min_size = min_size
         self.max_size = max_size
-        self.temperature = temperature
-        prior_size_scale_t = torch.tensor(prior_size_scale, dtype=torch.float32)
-        # compute the inverse softplus scale
-        prior_spi_scale = torch.log(torch.exp(prior_size_scale_t) - 1.0)
-        self.posterior, self.prior = self._build_qp(
-            init_loc=prior_size_loc,
-            init_spi_scale=prior_spi_scale
+        self.temperature = torch.tensor(
+            temperature,
+            dtype=torch.float32,
+            requires_grad=False
         )
         self._tril = torch.tril(
-            torch.ones((self.max_size, self.max_size))
+            torch.ones((self.max_size, self.max_size), requires_grad=False)
         )
+        self._categories = torch.arange(
+            self.min_size,
+            self.max_size + 1,
+            dtype=torch.float32
+        )
+        prior_scale_t = torch.tensor(prior_scale, dtype=torch.float32)
+        prior_loc_t = torch.tensor(prior_loc, dtype=torch.float32)
+        # compute the inverse softplus scale
+        prior_spi_scale_t = torch.log(
+            torch.exp(prior_scale_t) - 1.0
+        )
+        self.prior = self._build_prior(prior_loc_t, prior_spi_scale_t)
+        self.var_loc = nn.Parameter(
+            prior_loc_t,
+            requires_grad=True
+        )
+        self.var_spi_scale = nn.Parameter(
+            prior_spi_scale_t,
+            requires_grad=True
+        )
+        self.posterior = self._build_posterior(self.var_loc, self.var_spi_scale)
 
-    def _build_qp(self, init_loc, init_spi_scale):
+    def _build_prior(self, init_loc, init_spi_scale):
         # build the prior distribution
         init_scale = nn.functional.softplus(init_spi_scale)
         tn_prior = TruncatedNormal(
@@ -35,31 +53,32 @@ class AdaptiveSize(nn.Module):
             low=self.min_size,
             high=self.max_size
         )
-        size_cats = torch.arange(self.min_size, self.max_size + 1, dtype=torch.float32)
-        logits_prior = tn_prior.log_prob(size_cats)
+        logits_prior = tn_prior.log_prob(self._categories)
         dist_prior = distributions.OneHotCategorical(logits=logits_prior)
+        return dist_prior
 
-        var_loc = torch.tensor(init_loc, dtype=torch.float32)
-        self.var_loc = nn.Parameter(var_loc, requires_grad=True)
-        var_spi_scale = init_spi_scale
-        self.var_spi_scale = nn.Parameter(var_spi_scale, requires_grad=True)
+    def _build_posterior(self, param_loc, param_spi_scale):
         tn_posterior = TruncatedNormal(
-            loc=self.var_loc,
-            scale=nn.functional.softplus(self.var_spi_scale),
+            loc=param_loc,
+            scale=nn.functional.softplus(param_spi_scale),
             low=self.min_size,
             high=self.max_size
         )
-        logits_posterior = tn_posterior.log_prob(size_cats)
+        logits_posterior = tn_posterior.log_prob(self._categories)
         dist_posterior = distributions.RelaxedOneHotCategorical(
             logits=logits_posterior,
             temperature=self.temperature
         )
-        return dist_posterior, dist_prior
+        return dist_posterior
 
     def forward(self, input: Tensor):
+        self.posterior = self._build_posterior(
+            param_loc=self.var_loc,
+            param_spi_scale=self.var_spi_scale
+        )
         # sample the size from the posterior distribution
         # and compute the soft mask from the lower triangular matrix
-        post_sample = self.posterior.sample(sample_shape=(input.shape[0],))
+        post_sample = self.posterior.rsample(sample_shape=(input.shape[0],))
         mask = post_sample @ self._tril
         res = input * mask
         return res
