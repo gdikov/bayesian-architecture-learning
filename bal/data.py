0de064dafc67d98c293cd7ef65cb7c65a0553996
Georgi Dikov <gvdikov@gmail.com>
2020-01-03 23:49:51 +0100

    dataset from the blog post

--- bal/data.py
+++ bal/data.py
@@ -2,14 +2,15 @@ import numpy as np
 
 
 def generate_1d_regression(
-        n_points=100,
-        domain=(-5.0, 5.0),
-        noise_std=0.0,
+        n_points=2000,
+        domain=(-2.0, 2.0),
+        noise_std=0.1,
         seed=None):
 
     def f(x):
         x = np.atleast_1d(x)
-        y = 0.3 * x + (0.15 * x + 0.3) * np.sin(np.pi * x)
+        y = np.sin(6 * x) + 0.4 * x**2 - 0.1 * x**3 - x * np.cos(9 * np.sqrt(np.exp(x)))
+        # y = 0.3 * x + (0.15 * x + 0.3) * np.sin(np.pi * x)
         return y
 
     rng = np.random.RandomState(seed)
