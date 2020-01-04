3d5e40894380a90268f6c0456e9bece84a4d7ba1
Georgi Dikov <gvdikov@gmail.com>
2020-01-04 01:09:40 +0100

    add a simple batch iterator

--- bal/data.py
+++ bal/data.py
@@ -17,3 +17,24 @@ def generate_1d_regression(
     xs = rng.uniform(*domain, size=n_points).astype(np.float32).reshape(-1, 1)
     ys = (f(xs) + rng.normal(0.0, noise_std, size=(n_points, 1))).astype(np.float32)
     return xs, ys
+
+
+def batch_generator(xs, ys, batch_size, shuffle=False, seed=None):
+    """Batch generator of a labeled dataset.
+
+    Notes:
+        If len(xs) % batch_size > 0 the batches will not be of equal length
+        and may even be larger than batch_size.
+    """
+    if len(xs) != len(ys):
+        raise ValueError("Length of xs and ys is not the same.")
+
+    rng = np.random.RandomState(seed)
+    indices = np.arange(len(xs))
+    if shuffle:
+        rng.shuffle(indices)
+
+    n_batches = len(xs) // batch_size
+    batch_indices = np.array_split(indices, n_batches)
+    for ids in batch_indices:
+        yield xs[ids], ys[ids]
